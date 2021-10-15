af47c55350a6d3bea587253d4181a29892a5e9e7
Gpoole15 <garretpoole3@gmail.com>
2021-10-14 21:21:48 -0500

    Wrote Client Side Login

---
+++ Client/script.js
@@ -0,0 +1,23 @@
+['usernameInput', 'passwordInput'].forEach(element => {
+    document.getElementById(element).addEventListener('keydown', event => {
+        if (event.key == 'Enter') {
+            authenticate();
+        }
+    })
+})
+
+function authenticate() {
+    alert(`Hello, ${document.getElementById('usernameInput').value}`);
+
+    $.post('/api/login', {username:document.getElementById('usernameInput').value, password:document.getElementById('usernameInput').value}, data => {
+        //document.body.innertext = JSON.stringify(data);
+        if (data.success) {
+            document.getElementById('submitButton').style.background = 'Green';
+            document.getElementById('submitButton').innerText = success;
+        }else{
+            document.getElementById('submitButton').style.background = 'red';
+            document.getElementById('submitButton').innerText = data.err;
+        }
+        
+    });
+}
